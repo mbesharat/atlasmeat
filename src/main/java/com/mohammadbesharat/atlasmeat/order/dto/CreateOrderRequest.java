58d4a77f7f78606163b5e4f4aeafe93869180918
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-10 19:48:56 -0700

    change to manage branches

--- src/main/java/com/mohammadbesharat/atlasmeat/order/dto/CreateOrderRequest.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/order/dto/CreateOrderRequest.java
@@ -4,9 +4,17 @@ import jakarta.validation.constraints.*;
 
 public record CreateOrderRequest(
     
-    @NotBlank String customerName,
-    @NotBlank @Email String customerEmail,
-    @NotBlank String customerPhone,
-    @NotBlank String orderDetails
+    @NotBlank (message = "customer name is required")
+    @Size (max = 100, message = "customer name must be less than or equal to 100 characters")
+    String customerName,
+    @NotBlank (message = "customer email is required")
+    @Email (message = "customer email must be a valid email")
+    String customerEmail,
+    @NotBlank (message = "customer phone is required")
+    @Size (max = 30, message = "customer phone must be equal to or else than 30 characters")
+    String customerPhone,
+    @NotBlank (message = "order details is required")
+    @Size (max = 2000, message = "order details must be equal to or less than 2k characters")
+    String orderDetails
 )
 {}
--- src/main/java/com/mohammadbesharat/atlasmeat/order/service/OrderService.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/order/service/OrderService.java
@@ -4,11 +4,8 @@ package com.mohammadbesharat.atlasmeat.order.service;
 import com.mohammadbesharat.atlasmeat.order.domain.Order;
 import com.mohammadbesharat.atlasmeat.order.dto.CreateOrderRequest;
 import com.mohammadbesharat.atlasmeat.order.dto.OrderResponse;
-
-import static com.mohammadbesharat.atlasmeat.order.exceptions.OrderExceptions.*;
 import com.mohammadbesharat.atlasmeat.order.exceptions.OrderNotFoundException;
 import com.mohammadbesharat.atlasmeat.order.repo.OrderRepository;
-
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,36 +15,30 @@ import org.springframework.data.domain.Pageable;
 public class OrderService {
     
     public final OrderRepository orderRepository;
-    
+    // public final DayLimitRepository dayLimitRepository;
+
+    //this initializes the values of order and dayLimit rep
     public OrderService(OrderRepository orderRepository){
         this.orderRepository = orderRepository;
-        
+        // this.dayLimitRepository = dayLimitRepository;
     }
+
     
     public Order createOrder(CreateOrderRequest req){
 
-        // validation checks
-        if (req.customerName == null || req.customerName.trim().isEmpty()){
-            throw missingName();
-        }
-        if (req.customerEmail == null || req.customerEmail.trim().isEmpty()){
-            throw missingEmail(); 
-        }
-        if (req.customerPhone == null || req.customerPhone.trim().isEmpty()){
-            throw missingPhone();
-        }
-        if (req.orderDetails == null || req.orderDetails.trim().isEmpty()){
-            throw missingDetails();
-        }
+       
 
         //create order object and set values
         Order order = new Order();
-        order.setCustomerEmail(req.customerEmail.trim());
-        order.setCustomerName(req.customerName.trim());
-        order.setCustomerPhone(req.customerPhone.trim());
-        order.setOrderDetails(req.orderDetails);
+
+        order.setCustomerEmail(req.customerEmail().trim());
+        order.setCustomerName(req.customerName().trim());
+        order.setCustomerPhone(req.customerPhone().trim());
+        order.setOrderDetails(req.orderDetails());
+
         //this has the order saved into the DB
         return orderRepository.save(order);
+
     }
 
     public Order findOrderById(Long id){
