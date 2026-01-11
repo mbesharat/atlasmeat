192fba62c427d8e7f6e29c7d3cde711f7c87e98b
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-10 23:43:35 -0700

    added a space in an error message

--- src/main/java/com/mohammadbesharat/atlasmeat/order/exceptions/OrderNotFoundException.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/order/exceptions/OrderNotFoundException.java
@@ -12,6 +12,6 @@ public class OrderNotFoundException extends RuntimeException{
     }
     
     public OrderNotFoundException(Long id){
-        super("Order not found with id" + id);
+        super("Order not found with id " + id);
     }
 }
