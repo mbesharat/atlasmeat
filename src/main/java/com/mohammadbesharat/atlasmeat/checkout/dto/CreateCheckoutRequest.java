bbc23bb591f63bebb3e67c26f03b3efa504d0f9f
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-16 16:31:23 -0700

    Addded dto CreateCheckoutRequest

---
+++ src/main/java/com/mohammadbesharat/atlasmeat/checkout/dto/CreateCheckoutRequest.java
@@ -0,0 +1,26 @@
+package com.mohammadbesharat.atlasmeat.checkout.dto;
+
+import com.mohammadbesharat.atlasmeat.order.dto.CreateOrderRequest;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
+public record CreateCheckoutRequest(
+
+    @NotBlank (message = "customer name is required")
+    @Size (max = 100, message = "customer name must be less than or equal to 100 characters")
+    String customerName,
+    @NotBlank (message = "customer email is required")
+    @Email (message = "customer email must be a valid email")
+    String customerEmail,
+    @NotBlank (message = "customer phone is required")
+    @Size (max = 30, message = "customer phone must be equal to or else than 30 characters")
+    String customerPhone,
+    @NotNull(message = "orders are required")
+    @Size(min = 1, message = "at least one order is required")
+    @Valid
+    List<CreateOrderRequest> orders
+
+){}
