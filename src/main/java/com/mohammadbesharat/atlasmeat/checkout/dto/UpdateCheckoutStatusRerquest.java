de3b8c612c06f5cae107ac2a48bc779e59b3f1fd
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-19 16:56:34 -0700

    Added notnull validation to updatecheckoutstatusrequest'

--- src/main/java/com/mohammadbesharat/atlasmeat/checkout/dto/UpdateCheckoutStatusRerquest.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/checkout/dto/UpdateCheckoutStatusRerquest.java
@@ -2,7 +2,10 @@ package com.mohammadbesharat.atlasmeat.checkout.dto;
 
 import com.mohammadbesharat.atlasmeat.checkout.domain.CheckoutStatus;
 
+import jakarta.validation.constraints.NotNull;
+
 public record UpdateCheckoutStatusRerquest(
+    @NotNull
     Long checkoutId,
     CheckoutStatus status
 ) {}
