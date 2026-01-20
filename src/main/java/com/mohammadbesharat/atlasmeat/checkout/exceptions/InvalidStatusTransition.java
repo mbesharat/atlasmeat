653d19198aec58019df74067e596444a913bc357
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-19 17:33:59 -0700

    Added InvalidStatusTransition exception file

---
+++ src/main/java/com/mohammadbesharat/atlasmeat/checkout/exceptions/InvalidStatusTransition.java
@@ -0,0 +1,19 @@
+package com.mohammadbesharat.atlasmeat.checkout.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.mohammadbesharat.atlasmeat.checkout.domain.CheckoutStatus;
+
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidStatusTransition extends RuntimeException {
+
+     public InvalidStatusTransition(String message){
+        super (message);
+    }
+    
+    public InvalidStatusTransition(CheckoutStatus current, CheckoutStatus newStatus){
+        super(current + " cannot be changed to " + newStatus);
+    }
+}
