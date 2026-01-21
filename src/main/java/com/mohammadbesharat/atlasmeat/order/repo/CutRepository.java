5903726b17612688b876020e87aa332b177abec5
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-21 15:39:05 -0700

    fixed cutrepo method

--- src/main/java/com/mohammadbesharat/atlasmeat/order/repo/CutRepository.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/order/repo/CutRepository.java
@@ -5,12 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mohammadbesharat.atlasmeat.order.domain.Cut;
-import com.mohammadbesharat.atlasmeat.order.domain.Order;
 
 ;
 
 public interface CutRepository extends JpaRepository<Cut, Long>{
     
-    Optional<Order> findByIdAndOrderId(Long id, Long orderId);
+    Optional<Cut> findByIdAndOrderId(Long id, Long orderId);
 
 }
