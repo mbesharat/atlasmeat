a0f94e98cd167626b764a13a715d44d187a7f733
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-27 14:47:25 -0700

    Added testcontainers integratointesetbase

---
+++ src/test/java/com/mohammadbesharat/atlasmeat/IntegrationTestBase.java
@@ -0,0 +1,29 @@
+package com.mohammadbesharat.atlasmeat;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+@Testcontainers
+@SpringBootTest(classes = AtlasmeatApplication.class)
+@ActiveProfiles("test")
+public abstract class IntegrationTestBase {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+        .withDatabaseName("testdb")
+        .withUsername("test")
+        .withPassword("test");
+
+    @DynamicPropertySource
+    static void datasourceProps(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+    }
+    
+}
