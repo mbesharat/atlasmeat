83223f3c058450014e4029f0691d08b72e2b185d
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-27 22:59:53 -0700

    Fix Testcontainers setup and remove duplicate context test

--- src/test/java/com/mohammadbesharat/atlasmeat/IntegrationTestBase.java
+++ src/test/java/com/mohammadbesharat/atlasmeat/IntegrationTestBase.java
@@ -1,18 +1,16 @@
 package com.mohammadbesharat.atlasmeat;
 
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@SpringBootTest(classes = AtlasmeatApplication.class)
+@SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class IntegrationTestBase {
 
     @Container
@@ -20,7 +18,7 @@ public abstract class IntegrationTestBase {
             new PostgreSQLContainer<>("postgres:16-alpine");
 
     @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
+    static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
