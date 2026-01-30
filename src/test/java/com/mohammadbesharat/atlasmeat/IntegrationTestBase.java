e91f2d99bca7c42405c8b65849380c2f9124b3b7
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-01-30 10:49:59 -0700

    optimize: reuse single Testcontainer across all test

--- src/test/java/com/mohammadbesharat/atlasmeat/IntegrationTestBase.java
+++ src/test/java/com/mohammadbesharat/atlasmeat/IntegrationTestBase.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
@@ -15,9 +14,12 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @ActiveProfiles("test")
 public abstract class IntegrationTestBase {
 
-    @Container
-    static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:16-alpine");
+    
+    static PostgreSQLContainer<?> postgres; 
+    static{
+            postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+            postgres.start();
+    }
 
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
