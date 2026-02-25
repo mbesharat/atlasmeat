604795bb5defded1a03646001e23258fe272e457
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-02-25 13:36:59 -0700

    add initial method to WorkflowService

--- src/main/java/com/mohammadbesharat/atlasmeat/workflow/service/WorkflowService.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/workflow/service/WorkflowService.java
@@ -1,22 +1,22 @@
 package com.mohammadbesharat.atlasmeat.workflow.service;
 
 
+import com.mohammadbesharat.atlasmeat.checkout.dto.CheckoutResponse;
+import com.mohammadbesharat.atlasmeat.checkout.dto.CreateCheckoutRequest;
 import com.mohammadbesharat.atlasmeat.checkout.service.CheckoutService;
-import com.mohammadbesharat.atlasmeat.order.service.OrderService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkflowService {
 
     private final CheckoutService checkoutService;
-    private final OrderService orderService;
 
-    public WorkflowService(
-            CheckoutService checkoutService,
-            OrderService orderService
-    ){
+    public WorkflowService(CheckoutService checkoutService){
         this.checkoutService = checkoutService;
-        this.orderService = orderService;
+    }
+
+    public CheckoutResponse tartCheckoutWorkflow(CreateCheckoutRequest request){
+        return checkoutService.createCheckout(request);
     }
 
 }
