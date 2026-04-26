4ef7b19acec513b6f57f3bce15242fe442160ee2
Mohammad Besharat <120741779+mbesharat@users.noreply.github.com>
2026-04-25 19:45:55 -0600

    feat: add CreateAppointmentRequest and AppointmentResponse DTOS

--- src/main/java/com/mohammadbesharat/atlasmeat/appointment/dto/CreateAppointmentRequest.java
+++ src/main/java/com/mohammadbesharat/atlasmeat/appointment/dto/CreateAppointmentRequest.java
@@ -1,4 +1,27 @@
 package com.mohammadbesharat.atlasmeat.appointment.dto;
 
-public record CreateAppointmentRequest() {
-}
+import com.mohammadbesharat.atlasmeat.appointment.domain.ContactType;
+import com.mohammadbesharat.atlasmeat.order.domain.AnimalType;
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
+public record CreateAppointmentRequest(
+
+        @NotBlank(message = "customer name is required")
+        @Size(max = 100, message = "customer name must be less than or equal to 100 characters")
+        String customerName,
+        @NotBlank (message = "customer email is required")
+        @Email(message = "customer email must be a valid email")
+
+        String customerEmail,
+        @NotBlank (message = "customer phone is required")
+        @Size(max = 12, message = "customer phone must be less than or equal to 12 characters")
+        String customerPhone,
+        @NotNull(message = "contact preference is required")
+        ContactType contactPreference,
+        @NotNull(message = "animal type is required")
+        AnimalType animalType,
+        @Min(value = 1, message = "animal count must be at least 1")
+        int animalCount,
+        LocalDate scheduledDate
+) {}
